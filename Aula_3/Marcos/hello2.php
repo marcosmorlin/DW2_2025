9ec92657793eba9ebdbfe35d3115a2c55817cefa
marcosmorlin <marcosmorlin1020@gmail.com>
2025-03-20 16:10:28 -0300

    k

--- Aula_3/Marcos/hello2.php
+++ Aula_3/Marcos/hello2.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Primeira Página PHP</title>
+    <link rel="stylesheet" href="bootstrap-5.3.2-dist/css/bootstrap.min.css">
 </head>
 <body>
 
