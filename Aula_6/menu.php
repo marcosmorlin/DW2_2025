0546f7c38936d9932306a7c89429ece9f4185e6c
marcosmorlin <marcosmorlin1020@gmail.com>
2025-03-21 08:38:32 -0300

    k

---
+++ Aula_6/menu.php
@@ -0,0 +1,7 @@
+<ul class="nav nav-pills">
+    <li class="nav-item"><a href="" class="nav-link">Início</a></li>
+    <li class="nav-item"><a href="" class="nav-link">Sobre</a></li>
+    <li class="nav-item"><a href="" class="nav-link">FAQs</a></li>
+    <li class="nav-item"><a href="" class="nav-link">Contato</a></li>
+    <li class="nav-item"><a href="Destino-login.php" class="nav-link active" aria-current="page">Entrar</a></li>
+</ul>
